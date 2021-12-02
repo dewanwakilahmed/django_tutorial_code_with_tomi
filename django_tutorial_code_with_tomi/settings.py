d80db5bd7432e2772d9cb59150adf96db60a0320
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2021-12-03 03:23:31 +0600

    URL | Apps | Template
    - URL Routing
    - Django Apps
    - Django Template Language

--- django_tutorial_code_with_tomi/settings.py
+++ django_tutorial_code_with_tomi/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'django_tutorial_code_with_tomi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
