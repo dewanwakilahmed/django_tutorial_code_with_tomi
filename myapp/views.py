b405ade2dde952b89266e41a9c90ab87e5d90bce
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2021-12-04 02:17:49 +0600

    Data | Word Counter | GET vs POST | Static
    - Sending Data to Template File
    - Building a Word Counter in Django
    - GET vs POST in Django
    - Static Files in Django

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,15 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'name': 'Patrick',
+        'age': 23,
+        'nationality': 'British',
+    }
+    return render(request, 'index.html', context)
+
+
+def counter(request):
+    text = request.POST['text']
+    amount_of_words = len(text.split())
+    return render(request, 'counter.html', {'amount': amount_of_words})
